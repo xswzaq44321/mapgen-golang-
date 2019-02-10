4cbf13bde76b5f0e59729a0b10ec2a42b6a6b9b3
Weber <xswzaq44321@gmail.com>
2019-02-11 03:07:57 +0800

    Optimizing circle drawing algo

--- fortune/fortune.go
+++ fortune/fortune.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"image"
+	"image/png"
+	"os"
+)
 
 func main() {
-	fmt.Println("fortune")
+	// Create a blank image 100x200 pixels
+	myImage := image.NewRGBA(image.Rect(0, 0, 10, 4))
+
+	// outputFile is a File type which satisfies Writer interface
+	outputFile, err := os.Create("test.png")
+	if err != nil {
+		// Handle error
+	}
+	myImage.Pix[0] = 255 // 1st pixel red
+	myImage.Pix[1] = 0   // 1st pixel green
+	myImage.Pix[2] = 0   // 1st pixel blue
+	myImage.Pix[3] = 255 // 1st pixel alpha
+
+	// Encode takes a writer interface and an image interface
+	// We pass it the File and the RGBA
+	png.Encode(outputFile, myImage)
+
+	// Don't forget to close files
+	outputFile.Close()
 }
--- mamemaki/mamemaki.go
+++ mamemaki/mamemaki.go
@@ -3,10 +3,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
 	"io/ioutil"
 	"log"
 	V "mapgen-golang-/voronoi"
+	"math"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -26,7 +31,7 @@ func main() {
 		y := rand.Intn(setting.Height-2*setting.MapPadding) + setting.MapPadding
 		if check(voronoi.Polygons, x, y) {
 			bar := V.Polygon{
-				Focus: V.Point{x, y},
+				Focus: V.Point{X: x, Y: y},
 				Edges: make([]V.Edge, 0),
 			}
 			voronoi.Polygons = append(voronoi.Polygons, bar)
@@ -35,7 +40,7 @@ func main() {
 		}
 	}
 
-	output(voronoi)
+	imgOutput(voronoi)
 }
 
 func output(m V.Voronoi) {
@@ -46,9 +51,54 @@ func output(m V.Voronoi) {
 	fmt.Printf("%s", outJson)
 }
 
+func imgOutput(m V.Voronoi) {
+	myImage := image.NewRGBA(image.Rect(0, 0, setting.Width, setting.Height))
+
+	outputFile, err := os.Create("result.png")
+	defer func() {
+		outputFile.Close()
+	}()
+
+	if err != nil {
+		fmt.Println("error: ", err)
+		return
+	}
+	for i := 0; i < setting.Width; i++ {
+		for j := 0; j < setting.Height; j++ {
+			myImage.Set(i, j, color.White)
+		}
+	}
+	for _, p := range m.Polygons {
+		fillCircle(myImage, p.Focus.X, p.Focus.Y, setting.PointSize, color.RGBA{255, 0, 0, 255})
+		//myImage.Set(p.Focus.X, p.Focus.Y, color.RGBA{255, 0, 0, 255})
+	}
+
+	png.Encode(outputFile, myImage)
+}
+
+func fillCircle(img *image.RGBA, x0, y0, R int, c color.RGBA) {
+	r := float64(R) / 2
+	err := 0.0
+	if R%2 == 0 {
+		err = 0.5
+	}
+	for x := math.Floor(r) - err; x >= -math.Floor(r); x-- {
+		// round half down y = ceil(x-0.5)
+		for y := math.Ceil(math.Sqrt(r*r-x*x)-0.5) - err; y >= -math.Ceil(math.Sqrt(r*r-x*x)-0.5); y-- {
+			dx := x0 + int(x+err)
+			dy := y0 + int(y+err)
+			if dx < 0 || dx >= img.Rect.Dx() || dy < 0 || dy >= img.Rect.Dy() {
+				continue
+			} else {
+				img.Set(dx, dy, c)
+			}
+		}
+	}
+}
+
 func check(polys []V.Polygon, x int, y int) bool {
 	for _, v := range polys {
-		if v.Focus.Distance(V.Point{x, y}) < float64(setting.PointMargin) {
+		if v.Focus.Distance(V.Point{X: x, Y: y}) < float64(setting.PointMargin) {
 			return false
 		}
 	}
@@ -61,4 +111,5 @@ type Setting struct {
 	PointCount  int
 	PointMargin int
 	MapPadding  int
+	PointSize   int
 }
